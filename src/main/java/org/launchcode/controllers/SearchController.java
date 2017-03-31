9d61c9ff8b73d7ffb585981103a8dce9d04a0f5b
Erin DeVries <erindevrieswebdev@gmail.com>
2017-03-31 12:55:58 -0500

    created handler method for search. created fragment. cleaned up formatting

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,12 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
 
 /**
  * Created by LaunchCode
@@ -16,6 +18,8 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
+    ArrayList<HashMap<String, String>> jobs = null;
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
@@ -23,5 +27,17 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchTerm, @RequestParam String searchType) {
 
+        model.addAttribute("columns", ListController.columnChoices);
+        if(searchType.equals("all")){
+            model.addAttribute("jobs", JobData.findByValue(searchTerm));
+        }else {
+            model.addAttribute("jobs", JobData.findByColumnAndValue(searchType, searchTerm));
+
+        }
+        return "search";
+
+    }
 }
